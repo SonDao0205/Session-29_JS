e4b15f3f42f7c6e307ea2d6e24d026e7dcb3a287
SonDao0205 <daotruongson2006@gmail.com>
2025-03-18 08:35:21 +0700

    ex

--- Session-29_Ex-05.js
+++ Session-29_Ex-05.js
@@ -1,39 +1,20 @@
 let choice
 const accounts = []
 
-const checkEmail = (username) => {
-    return username.includes("@") && (username.endsWith(".com") || username.endsWith(".vn"));
+const checkEmail = (email) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|vn)$/;
+    return emailRegex.test(email)
 }
 
 const checkPassword = (password) => {
-    if (password.length < 6){ 
-        return false
-    };
-    let upperCase = false;
-    let lowerCase = false;
-    let special = false;
-
-    for (const char of password) {
-        if (char >= 'A' && char <= 'Z'){
-            upperCase = true;
-        }
-        else if (char >= 'a' && char <= 'z'){
-            lowerCase = true;
-        }
-        else if ((char >= '!' && char <= '/') || (char >= ':' && char <= '@') || (char >= '[' && char <= '`') || (char >= '{' && char <= '~')){
-            special = true;
-        }
-        if (upperCase && lowerCase && special){
-            return true;
-        }
-    }
-    return false;
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[\W_]).{6,}$/;
+    return passwordRegex.test(password)
 }
 
 const register = (accounts) => {
-    const username = prompt(`Nhap username`)
+    const email = prompt(`Nhap email`)
     const password = prompt(`Nhap mat khau`)
-    if (!checkEmail(username)) {
+    if (!checkEmail(email)) {
         console.log("Email khong hop le");
         return;
     }
@@ -42,20 +23,20 @@ const register = (accounts) => {
         return;
     }
 
-    const existUser = accounts.find((acc) => acc.username === username);
+    const existUser = accounts.find((acc) => acc.email === email);
     if (existUser) {
         console.log("Email da ton tai");
         return;
     }
 
-    accounts.push({ username, password });
+    accounts.push({ email, password });
     console.log("Dang ky thanh cong");
 }
 
 const login = (accounts) => {
-    const username = prompt("Nhập email:");
+    const email = prompt("Nhập email:");
     const password = prompt("Nhập mật khẩu:");
-    const user = accounts.find((acc) => acc.username === username && acc.password === password);
+    const user = accounts.find((acc) => acc.email === username && acc.password === password);
     if (user) {
         console.log("Dang nhap thanh cong");
     } else {
@@ -84,4 +65,5 @@ do {
                 break;
         }
 } while (choice !== 3);
+
 
